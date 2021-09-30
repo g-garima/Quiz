ad33b5395cc247a6463198dc87ba2acd30e172c2
Garima <77682417+g-garima@users.noreply.github.com>
2021-09-30 21:24:04 +0530

    Create questions.js

---
+++ javascript/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What's Pam's favorite flavor of yogurt?",
+    answer: "Mixed berry",
+    options: [
+      "Vanilla",
+      "Straw-berry",
+      "Mixed berry",
+      "Chocolate"
+    ]
+  },
+    {
+    numb: 2,
+    question: "A foreign exchange student lived with Michael when he was young. What did the foreign exchange student take from Michael back to what was formerly Yugoslavia?",
+    answer: "All of his blue jeans",
+    options: [
+      "All of his blue jeans",
+      "All of his shorts",
+      "His favorite shoes",
+      "All of his shirts"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does Kevin suggest Dwight put in his gun holster?",
+    answer: "A banana",
+    options: [
+      "A cell phone",
+      "A banana",
+      "A toy gun",
+      "A cupcake"
+    ]
+  },
+    {
+    numb: 4,
+    question: "During the episode, Prince Family Paper, what's the secret signal that Dwight and Michael agree on using?",
+    answer: "Licking your lips",
+    options: [
+      "Raising your eyebrows",
+      "Licking your lips",
+      "Coughing loudly",
+      "Winking"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does Michael make a ton of for him and Pam on the morning of the first day of the Michael Scott Paper Company?",
+    answer: "French toast",
+    options: [
+      "Waffles",
+      "Pancakes",
+      "French toast",
+      "Egg rolls"
+    ]
+  },
+];
